0798da52d3ad7c90f4fa5eb56c0b086a9462da0a
aiibe <souksavanh.sy@gmail.com>
2022-01-31 23:11:56 +0100

    Add pagination

--- src/containers/Cards.tsx
+++ src/containers/Cards.tsx
@@ -1,9 +1,12 @@
-import { useState } from "react";
+import { ChangeEventHandler, useState } from "react";
 import Card from "../components/Card";
 import Filter from "../components/Filter";
 import { useAppSelector } from "../redux/hooks";
 
 const Cards = () => {
+  const itemsPage = [4, 8, 12];
+  const [currentPage, setCurrentPage] = useState(0);
+  const [itemsPerPage, setItemsPerPage] = useState(itemsPage[0]);
   const [filterSelection, setFilterSelection] = useState<string[]>([]);
   const movies = useAppSelector((state) => state.movies);
 
@@ -16,12 +19,24 @@ const Cards = () => {
     allMovies = movies.filter((m) => filterSelection.includes(m.category));
   }
 
+  // Pagination
+  const pagesCount = Math.ceil(allMovies.length / itemsPerPage);
+  allMovies = movies.slice(
+    itemsPerPage * currentPage,
+    itemsPerPage * (currentPage + 1)
+  );
+
   // Event Handlers
   const handleSelected = (opt: string) => {
     const newFilter = filterSelection.includes(opt)
       ? filterSelection.filter((f) => f !== opt)
       : [...filterSelection, opt];
     setFilterSelection(newFilter);
+  };
+
+  const handleSelectPage: ChangeEventHandler<HTMLSelectElement> = (e) => {
+    setItemsPerPage(parseInt(e.target.value));
+    setCurrentPage(0);
   };
 
   return (
@@ -31,10 +46,42 @@ const Cards = () => {
         selected={handleSelected}
         actives={filterSelection}
       />
-      <div className="grid gap-4 md:grid-cols-2 sm: grid-cols-1 lg:grid-cols-3">
+      <div className="grid gap-4 md:grid-cols-2 sm: grid-cols-1">
         {allMovies?.map((item, i) => (
           <Card key={i} item={item} />
         ))}
+      </div>
+
+      {/* Paginate */}
+      <div className="flex items-center justify-between my-2">
+        <div className="flex items-center">
+          <span className="mr-2">Items per page</span>
+          <select
+            className="border"
+            value={itemsPerPage}
+            onChange={handleSelectPage}
+          >
+            {itemsPage.map((opt) => (
+              <option key={opt} value={opt}>
+                {opt}
+              </option>
+            ))}
+          </select>
+        </div>
+        <div className="">
+          {pagesCount > 1 &&
+            [...Array(pagesCount)].map((_, i) => (
+              <button
+                key={i}
+                onClick={() => setCurrentPage(i)}
+                className={`px-3 ml-1 bg-gray-300 hover:bg-gray-400 text-white ${
+                  currentPage === i && "bg-gray-500"
+                }`}
+              >
+                {i + 1}
+              </button>
+            ))}
+        </div>
       </div>
     </div>
   );
